2acd92bb48852644f251249b6f97ace9690f56ce
Yuliya <darkwader@mail.ru>
2023-02-12 17:55:38 +0300

    Array2

---
+++ Array2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 15, 4, 86, 22, 89, 69, 4, 78 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        // break; - чтобы искал только первый индекс, а не все равные файнд
+    }
+    index++;
+}
